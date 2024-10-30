2c1f310a2088f82d8be463d2e897a3c0f09faf53
armin <kuffer.armin@gmail,com>
2024-10-30 17:07:12 +0100

    nm

--- lagrange.py
+++ lagrange.py
@@ -14,8 +14,21 @@ def LagrangePolynom(x,n,x_node,f_node):
     poly = 0
     for i in range(n+1):
         poly = poly+np.multiply(f_node[i],LagrangeBasis(x,n,i,x_node))
-    print(poly)
     plt.plot(x,poly,color="yellow",linewidth=1)
+    plt.plot(x_node,f_node,'o',color='black')
+    plt.title('Lagrangian polynome of order 4')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.grid(linewidth=.05)
+    plt.plot()
+    plt.plot(x,fx,color="black",linewidth=1)
+    plt.show()
+    return poly
+def forwarddifference(x,n,x_node,f_node):
+    h = 10**(-5)
+    fx0 = ((LagrangePolynom(x,n,x_node,f_node)+h)-(LagrangePolynom(x,n,x_node,f_node)))/h
+    print(fx0)
+    plt.plot(x,fx0,color="yellow",linewidth=1)
     plt.plot(x_node,f_node,'o',color='black')
     plt.title('Lagrangian polynome of order 4')
     plt.xlabel('x')
@@ -28,4 +41,5 @@ x_node = [0,1,2,3,4]
 f_node = (x_node/(np.add(1,x_node)))**5
 x = np.linspace(0,4,500)
 fx = (x/(np.add(1,x)))**5
-LagrangePolynom(x,4,x_node,f_node)
+#LagrangePolynom(x,4,x_node,f_node)
+forwarddifference(x,4,x_node,f_node)

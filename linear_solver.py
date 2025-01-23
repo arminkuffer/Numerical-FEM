465d00e035759ec27a9d84f87976a7e105744c1a
armin <kuffer.armin@gmail,com>
2025-01-23 22:47:39 +0100

    nlin + linsolv complete

--- linear_solver.py
+++ linear_solver.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time as t 
 def solveGauss(A, b):
     n = len(b)
     x = np.zeros((n,))
@@ -25,6 +25,7 @@ def solveG(A,b,x0,rtol,itermax):
         x += alphak*r
         r -= alphak*v
         k+=1
+    print(k)
     return x
 
 def solveCG(A,b,x0,rtol,itermax):
@@ -42,7 +43,26 @@ def solveCG(A,b,x0,rtol,itermax):
         p = r_new + beta*p
         r = r_new
         k+=1
+    print(k)
     return x
+
+n = 300
+phi = 5.01
+b = np.ones((n,))
+A = np.diag(np.full((n),phi)) -2*np.diag(np.ones((n-1)),k=1) - 2*np.diag(np.ones((n-1)),k=-1)
+time_start = t.time()
+sol = solveGauss(A,b)
+time_stop = t.time()
+print(sol)
+print(time_stop-time_start)
+time_start = t.time()
+sol = solveG(A,b,np.zeros((n,)),0.0000001,1000)
+time_stop = t.time()
+print(sol)
+print(time_stop-time_start)
+
+
+    
 """print(solveCG(np.array([[10.0, 2.0, 10.0],[ 2.0, 40.0, 8.0],[ 10.0, 8.0, 60.0]]),np.array([1.0, 1.0, 2.0]),np.array([0.0, 0.0, 0.0]), 10**(-7), 1000))
 print(solveG(np.array([[10.0, 2.0, 10.0],[ 2.0, 40.0, 8.0],[ 10.0, 8.0, 60.0]]),np.array([1.0, 1.0, 2.0]),np.array([0.0, 0.0, 0.0]), 10**(-7), 1000))
 print(solveGauss(np.array([[10.0,2.0,2.0],[3.0,4.0,4.0],[1.0,8.0,4.0]]),np.array([1.0,1.0,2.0])))"""
--- nlinear.py
+++ nlinear.py
@@ -1,15 +1,33 @@
+
 import numpy as np
-from lagrange2D import linquadderivref
+from lagrange2D import linquadderivref,linquadref
 from triagplot import quadplot
 from integration import gx2dref,gw2dref,getJacobian
 import linear_solver as linsolve
+"""def f(x):
+    return np.arctan(x)
+def derivf(x):
+    return 1/(1+x**2)
+def newton1D(x0):
+    xn = x0+1
+    n=0
+    while np.abs(f(x0)-f(xn)) >= 10**(-12):
+        xn = x0
+        x0 = x0-f(x0)/derivf(x0)
+        n+=1
+    return x0,n
+
+print(newton1D(1.0))"""
+
 #Variables
 Lambda = 48
 T1 = 600
 T2 = 300
-r = 0.02+0.06 #r = 0.08 kleinster Radius für den gilt T15 < 450 (Maximalwert für y = h)
+r = 0.01 #r = 0.08 kleinster Radius für den gilt T15 < 450 (Maximalwert für y = h)
 b = 0.3
 h = 0.3
+c_1 = 10**6
+c_2 = 10**3
 nodes = np.array([[0,0],
                   [b/3,0],
                   [(2/3)*b,0],
@@ -50,16 +68,22 @@ dbc = np.array([[1,T1],
                 [18,T2]])
 
 #Functions:
-def evaluate_stat(elenodes, gpx, gpw):
+def evaluate_stat(elenodes, gpx, gpw,T):
     elemat = np.zeros((4, 4))
+    elevec = np.zeros(4)
+    Teq = 0
     for k in range(len(gpx)):
         J, detJ, invJ = getJacobian(elenodes, gpx[k][0], gpx[k][1])
         for i in range(elemat.shape[0]):
+            N_i = linquadref(gpx[k][0],gpx[k][1])[i]
+            gradN_i = np.matmul(linquadderivref(gpx[k][0], gpx[k][1])[i], invJ)
             for j in range(elemat.shape[1]):
-                gradN_i = np.matmul(linquadderivref(gpx[k][0], gpx[k][1])[i], invJ)
+                N_j = linquadref(gpx[k][0],gpx[k][1])[j]
                 gradN_j = np.matmul(linquadderivref(gpx[k][0], gpx[k][1])[j], invJ)
-                elemat[i][j] += Lambda * (gradN_i @ gradN_j) * detJ * gpw[k]   
-    return elemat, np.zeros(4)
+                elemat[i][j] += Lambda * (gradN_i @ gradN_j) * detJ * gpw[k] 
+                Teq += N_j*T[j]
+            elevec[i] += -N_i * c_1 * np.exp(-(c_2/(Teq)))*detJ*gpw[k]
+    return elemat,elevec
 
 def assemble(elemat,elevec,sysmat,rhs,ele):
     for i in range(4):
@@ -68,14 +92,16 @@ def assemble(elemat,elevec,sysmat,rhs,ele):
             sysmat[int(ele[i]-1)][int(ele[j]-1)] += elemat[i][j]
     return sysmat,rhs
 
-def assignDBC(sysmat,rhs,dbc):
+def assignDBC_nlin(sysmat,rhs,dbc):
     for i in range(dbc.shape[0]):
         rhs[int(dbc[i][0]-1)] = dbc[i][1]
         for k in range(sysmat.shape[1]):
                 if k == int(dbc[i][0]-1):
                     sysmat[int(dbc[i][0]-1)][k] = 1
                 else:
-                    sysmat[int(dbc[i][0]-1)][k] = 0        
+                    sysmat[int(dbc[i][0]-1)][k] = 0 
+                    rhs += -dbc[i][1]*sysmat[k][int(dbc[i][0]-1)]   
+                    sysmat[k][int(dbc[i][0]-1)]  = 0   
     return sysmat,rhs
 
 def solve(nodes,elements,dbc):
@@ -87,13 +113,13 @@ def solve(nodes,elements,dbc):
             assemb = assemble(eval[0],eval[1],np.zeros((nodes.shape[0],nodes.shape[0])),np.zeros((nodes.shape[0],)),e)
         else:
             assemb = assemble(eval[0],eval[1],assemb[0],assemb[1],e)
-    assign = assignDBC(assemb[0],assemb[1],dbc)
+    assign = assignDBC_nlin(assemb[0],assemb[1],dbc)
     sol = linsolve.solveGauss(assign[0],assign[1])
     sol = np.array(sol).flatten()
     return sol
 
+
 sol = solve(nodes,elements,dbc)
 print(sol)
 quadplot(nodes,elements-1,sol)
-
 
